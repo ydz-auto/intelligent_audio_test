34ce87ff99cca1cc7762ef4a90c54e3cde24a1ba
lijiahao <lijiahao69@huawei.com>
2026-05-21 09:27:26 +0800

    refactor(filter): 移除未使用的标签过滤组件
    删除 DimensionFilterPanel、GroupFilterPanel、TagFilterPanel 组件，
    这些组件已被新的过滤实现替代

--- Intelligent-Audio-TEST/frontend/src/components/common/filter/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as TagFilterPanel } from './TagFilterPanel.vue';
-export { default as DimensionFilterPanel } from './DimensionFilterPanel.vue';
-export { default as GroupFilterPanel } from './GroupFilterPanel.vue';
